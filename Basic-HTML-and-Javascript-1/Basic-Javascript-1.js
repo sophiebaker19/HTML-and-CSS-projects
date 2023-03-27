2fae298a79cb4bbbe38f5ee54e1a26ddbfd96a97
sophiebaker19 <sophiebaker2004@gmail.com>
2023-03-27 17:11:22 +0100

    Basic HTML and JavaScript
    Made basic HTML and JavaScript code

---
+++ Basic-HTML-and-Javascript-1/Basic-Javascript-1.js
@@ -0,0 +1,6 @@
+alert ("Hello World, This is an alert message!")
+
+function My_First_Function() {
+var String="Kiss me, I'm Irish!";
+var result=String.fontcolor("green");
+document.getElementById("Irish").innerHTML=result;}
